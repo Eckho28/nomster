dc11d703109a23294b52f18564e530ff21ed9db7
eckho28 <eckho28@gmail.com>
2014-06-04 21:43:00 +0000

    Added user profile pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,8 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :delete]
 	
 	def index
-		@places = Place.paginate(:page => params[:page], :per_page => 15, :order => 'created_at DESC')
-	end
+		@places = Place.paginate(:page => params[:page], :per_page => 15, :order => 'created_at DESC')	 
+  end
 
 	def new
 		@place = Place.new
